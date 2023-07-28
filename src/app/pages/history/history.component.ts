3a4a2574138b431c33f6e3b67c63f0ce2a83b88b
smimt-hirpara <hsmit695@gmail.com>
2023-07-28 07:50:25 +0530

    product cards

--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-history',
@@ -6,4 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./history.component.scss']
 })
 export class HistoryComponent {
+  constructor(public authService: AuthService, public ThemeService: ThemeService) {
+
+    /*----------- Check User Authorize Or Not -----------*/
+    if (!this.authService.authorize) {
+      this.authService.navigateLoginForm();
+    }
+  }
 }
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -15,4 +15,12 @@ export class ProductsComponent {
       this.authService.navigateLoginForm();
     }
   }
+
+  products = [
+    {img:'assets/product-images/black_hoodi.png',name:'Tishart', price:'200', size:'sm'},
+    {img:'assets/product-images/blue_shirt.png',name:'Tishart', price:'150', size:'sm'},
+    {img:'assets/product-images/pink_tshirt.png',name:'Tishart', price:'180', size:'lg'},
+    {img:'assets/product-images/red_tshirt.png',name:'Tishart', price:'120', size:'lg'},
+    {img:'assets/product-images/white_teshirt.png',name:'Tishart', price:'500', size:'sm'},
+  ]
 }
